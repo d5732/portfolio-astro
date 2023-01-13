884a5c86f49008682e8459f08090c09b49017bd7
d5732 <denwa.dev@gmail.com>
2023-01-13 17:34:19 -0600

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,49 @@
+export const info = {
+    baseUrl: "",
+    name: "Dennis Drueck",
+    jobDescription: "Full-Stack Developer",
+    about: `With 4 years of software development experience and 16 years of IT experience, I am a talented and seasoned professional. Through my successful projects and certifications, I have demonstrated mastery of computer science fundamentals and full-stack application development. My strong focus on teamwork and collaboration helps me ensure that goals are well defined and deadlines are met. Given my diverse background spanning multinational corporations, startups, and freelancing, I can readily excel in any environment.`,
+
+    experience: [
+        {
+            name: "Company Name",
+            location: "Location",
+            startDate: "2010",
+            endDate: "2020",
+            description: ["Bullet 1", "Bullet 2"],
+        },
+    ],
+
+    education: [
+        {
+            name: "FastTrack Developer Program",
+            location: "Memphis, TN (Remote)",
+            startDate: "2022",
+            endDate: "2023",
+            description: ["Bullet 1", "Bullet 2"],
+        },
+        {
+            name: "University of Illinois - Chicago",
+            location: "Chicago, IL",
+            startDate: "2008",
+            endDate: "2012",
+            description: ["Bullet 1", "Bullet 2"],
+        },
+    ],
+
+    socialMedia: {
+        github: "https://github.com/d5732",
+        email: "Denwa.dev@gmail.com",
+        linkedin: "https://www.linkedin.com/in/dennis-d-58764754/",
+    },
+
+    projects: [
+        {
+            title: "Astro Portfolio",
+            isFeatured: true,
+            thumbnail: "/assets/images/astro-portfolio.png",
+            githubUrl: "https://github.com/d5732/portfolio-astro",
+            liveUrl: "",
+        },
+    ],
+};
