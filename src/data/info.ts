d1422757c6e13959079903b3100e3c336a610e7e
d5732 <denwa.dev@gmail.com>
2024-04-12 21:12:19 -0500

    cleanup

--- src/data/info.ts
+++ src/data/info.ts
@@ -79,10 +79,10 @@ export const info = {
       startDate: "12/2022",
       endDate: "4/2023",
       description: [
-        "Lead design and implementation of solutions for workflow automation, geolocation, and image processing, in collaboration with the CTO and the product manager.",
-        "Reduced support requests for cross-platform issues by implementing responsiveness and input validation for 50+ web pages using PHP, Blade, jQuery, and Bootstrap.",
-        "Increased SERP ranking and page speed via network and asset optimization strategies.",
-        "Automated time consuming tasks related to logo color extraction by scraping CDNs and analyzing images with JavaScript, Node.js, color-thief and tinycolor2.",
+        "Led the design and implementation of solutions for workflow automation, geolocation, and image processing in collaboration with the CTO and product manager.",
+        "Reduced cross-platform support requests by implementing responsiveness and input validation across 50+ web pages using PHP, Blade, jQuery, and Bootstrap.",
+        "Improved SERP ranking and page speed through network and asset optimization strategies.",
+        "Automated time-consuming tasks related to logo color extraction by scraping CDNs and analyzing images with JavaScript, Node.js, color-thief and tinycolor2.",
       ],
     },
     {
