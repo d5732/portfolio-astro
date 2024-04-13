0b131cfc8fd13abff63bdf83d8f2b24317ae4873
d5732 <denwa.dev@gmail.com>
2024-04-12 23:40:52 -0500

    ordering

--- src/data/info.ts
+++ src/data/info.ts
@@ -49,13 +49,13 @@ export const info = {
       startDate: "4/2023",
       endDate: "Present",
       description: [
-        "Enhance release velocity and impact by identifying the most valuable features and persuading stakeholders to minimize scope creep.",
-        "Optimize event processing between interconnected systems, eliminating costly delays and enhancing data integrity and system stability within a complex microservice architecture.",
-        "Foster team skill development through effective mentorship and tactful, constructive feedback, aligned with organizational core values and engineering best practices.",
         "Lead large-scale deliveries by spearheading the scoping phase, prioritizing requirements, and defining objectives, timelines, and milestones.",
-        "Conduct root cause analysis (RCA) using various tools, including VS Code debugger, Splunk, GCP Console, AWS Console, DataGrip, and Chrome DevTools.",
+        "Enhance release velocity and impact by identifying the most valuable features.",
+        "Foster team skill development through effective mentorship and tactful, constructive feedback, aligned with organizational core values and engineering best practices.",
+        "Optimize event processing between interconnected systems, eliminating costly delays and enhancing data integrity and system stability within a complex microservice architecture.",
         "Provide expertise in design patterns for CI/CD and containerization, employing multi-step builds in Concourse CI to improve replication for horizontally-scaled systems, and reduce time to deploy new builds by a factor of 10.",
         "Ensure compliance with application development and cloud security best practices, safeguarding patient and organization data privacy and PHI compliance. ",
+        "Conduct root cause analysis (RCA) using various tools, including VS Code debugger, Splunk, GCP Console, AWS Console, DataGrip, and Chrome DevTools.",
       ],
     },
     {
